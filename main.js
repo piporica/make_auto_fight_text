0efbb9300442c1b3230f319adc7c1d0075769a4f
unknown <hy.bang@lotteworld.com>
2021-09-03 17:49:07 +0900

    edit message

--- main.js
+++ main.js
@@ -2,6 +2,37 @@ let turn;
 let P1 = {};
 let P2 = {};
 
+function BattleStart(){
+    PlayerSet();
+    if(!checkAllStatOk()) return;
+
+    checkTurn();
+
+}
+
+function checkAllStatOk(){
+    function check(value){
+        return 0 <= value && value <= 8
+    }
+    str = "";
+    if(!check(P1.atk)) str += "\nP1의 atk값을 확인해주세요.";
+    if(!check(P1.def)) str += "\nP1의 def값을 확인해주세요.";
+    if(!check(P1.dex)) str += "\nP1의 dex값을 확인해주세요.";
+    if(!check(P1.str)) str += "\nP1의 str값을 확인해주세요.";
+    
+    if(!check(P2.atk)) str += "\nP2의 atk값을 확인해주세요.";
+    if(!check(P2.def)) str += "\nP2의 def값을 확인해주세요.";
+    if(!check(P2.dex)) str += "\nP2의 dex값을 확인해주세요.";
+    if(!check(P2.str)) str += "\nP2의 str값을 확인해주세요.";
+
+    let isOk = check(P1.atk) && check(P1.def) && check(P1.dex) && check(P1.str)
+    &&check(P2.atk) && check(P2.def) && check(P2.dex) && check(P2.str)
+    
+    if(!isOk) alert(str);
+
+    return isOk;
+}
+
 function PlayerSet(){
     P1 = {
         name : "P1",
@@ -19,20 +50,6 @@ function PlayerSet(){
     }
 }
 
-function checkAllStatOk(){
-    function check(value){
-        return "0" <= value && value <= "8"
-    }
-    if(!check(P1.atk)) alert("P1의 atk값을 확인해주세요.");
-    if(!check(P1.def)) alert("P1의 def값을 확인해주세요.");
-    if(!check(P1.dex)) alert("P1의 dex값을 확인해주세요.");
-    if(!check(P1.str)) alert("P1의 str값을 확인해주세요.");
-    
-    if(!check(P2.atk)) alert("P2의 atk값을 확인해주세요.");
-    if(!check(P2.def)) alert("P2의 def값을 확인해주세요.");
-    if(!check(P2.dex)) alert("P2의 dex값을 확인해주세요.");
-    if(!check(P2.str)) alert("P2의 str값을 확인해주세요.");
-}
 
 function checkTurn(){
     let rst = p1.name + "의 민첩 : " + P1.dex + " " + p1.name + "의 민첩 : " + P2.dex;
